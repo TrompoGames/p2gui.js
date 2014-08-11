e5b7065ffa03970dba87a4e5a3595cba5a919ff4
Dario Segura <dario@getsetgames.com>
2014-08-11 15:42:42 -0400

    -[PANEL] Implemented document --> configuration panel event handlers.

--- PhotoshopCC_Panel/ExtensionContent/js/document.js
+++ PhotoshopCC_Panel/ExtensionContent/js/document.js
@@ -1,7 +1,39 @@
 (function()
 {
-	var configuration = P2GUI.eventManager.on("onEnter_document_configuration", function()
-	{
-		updateMetadataAndGUI(P2GUI.document.configuration, P2GUI.document.configurationDefaults);
-	});
+	/* configuration */
+	var configuration = {
+			
+			onEnter					: P2GUI.eventManager.on("onEnter_document_configuration", function()
+					{
+						updateMetadataAndGUI(P2GUI.document.configuration, P2GUI.document.configurationDefaults);
+					}),
+			
+			autoClassTypeChanged	: P2GUI.eventManager.on("onChanged_" + P2GUI.document.configuration.autoClassType, function(newValue)
+					{
+						/* if it's not set to custom hide the corresponding text field, otherwise show it */
+						var element = jQuery("#" + P2GUI.document.configuration.autoClassDescriptor);
+						if (newValue == P2GUI.value.custom)
+						{
+							element.parent().show();
+						}
+						else
+						{
+							element.parent().hide();
+						}
+					}),
+					
+			classFieldsTypeChanged	: P2GUI.eventManager.on("onChanged_" + P2GUI.document.configuration.classFieldsType, function(newValue)
+					{
+						/* if it's not set to custom hide the corresponding text field, otherwise show it */
+						var element = jQuery("#" + P2GUI.document.configuration.classFieldsDescriptor);
+						if (newValue == P2GUI.value.custom)
+						{
+							element.parent().show();
+						}
+						else
+						{
+							element.parent().hide();
+						}
+					}),
+	}
 })();
