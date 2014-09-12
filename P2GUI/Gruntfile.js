7f78f94c81b0f7a69ce981b94e7f4ddc8d2497a4
Dario Segura <dario@getsetgames.com>
2014-09-12 12:34:07 -0400

    -[GRUNT] Added initial grunt support

---
+++ P2GUI/Gruntfile.js
@@ -0,0 +1,49 @@
+/*global module:false*/
+module.exports = function(grunt) {
+	
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	
+	// source files //
+	var sourceFiles = [
+		'<%= pkg.sourceFolder %>/P2GImporter.js',
+	];
+
+	// Project configuration.
+	grunt.initConfig({
+		// Metadata.
+		pkg: grunt.file.readJSON('package.json'),
+		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+		'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+		'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+		'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+		' All rights reserved. */\n',
+		// Task configuration.
+		concat: {
+			options: {
+				banner: '<%= banner %>',
+				stripBanners: true
+			},
+			dist: {
+				src: sourceFiles,
+				dest: '<%= pkg.outputFolder %>/<%= pkg.outputName %>.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '<%= banner %>',
+				compress: true,
+				mangle: true
+			},
+			dist: {
+				src: '<%= concat.dist.dest %>',
+				dest: '<%= pkg.outputFolder %>/<%= pkg.outputName %>.min.js'
+			}
+		}
+	});
+
+	// Default task.
+	grunt.registerTask('default', ['concat', 'uglify']);
+
+};
